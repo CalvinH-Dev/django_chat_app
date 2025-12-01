69268d455fdea9d4d3961e5fd5e627442671525b
CalvinH-Dev <hanisch.calvin@gmail.com>
2025-12-01 16:51:57 +0100

    add: basic structure of urls for GET and POST requests

--- chat_app/views.py
+++ chat_app/views.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import View
 
 # Create your views here.
+
+
+class ChatView(View):
+    def get(self, request: HttpRequest):
+        return HttpResponse("ok get")
+
+    def post(self, request: HttpRequest):
+        if not request.body:
+            return HttpResponse(status=204)
+        return HttpResponse("ok post")
--- core/settings.py
+++ core/settings.py
@@ -43,12 +43,23 @@ INSTALLED_APPS = [
     "chat_app",
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:8000",
+    "http://127.0.0.1:8000",
+]
+
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
